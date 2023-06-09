11fbe98eab27bdf76d57ab82c5e09b9ef07cbbce
fmuamarr <fadillahmuamar@gmail.com>
2023-06-10 00:41:54 +0700

    Char animation
    animasi karakter, perlu adjustment agar supaya lebih

--- Assets/Dicky Project/script/movement.cs
+++ Assets/Dicky Project/script/movement.cs
@@ -5,15 +5,29 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float kecepatan = 7f;
+    public float speed = 10f;
     [SerializeField] TextMeshProUGUI _nScore;
     [SerializeField] TextMeshProUGUI _nHighScore;
     public GameObject player;
     private GameObject menCam;
     private CharacterController carkon;
-    private float  highscore,x,y,z;
+    public float  highscore,x,y,z;
     private int score;
     // Start is called before the first frame update
+
+    //MOVEMENT
+    public CharacterController controller;
+    [SerializeField] private Transform groundcheck;
+    [SerializeField] private float ground_Distance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    public bool isGround;
+    Vector3 velocity;
+     private float gravitasi = 20f;
+    private float speed_jump = 2f;
+    public float speed_lari = 100f;
+
+
+
     void Start()
     {
         menCam = Camera.main.gameObject;
@@ -29,16 +43,13 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        gravity();
         bergerak();
+        lari();
+        jump();
     }
 
-    private void bergerak()
-    {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-        Vector3 gerakan = transform.right * x + transform.forward * z;
-        carkon.Move(gerakan * kecepatan * Time.deltaTime);
-    }
+  
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Obstacle")
@@ -55,5 +66,42 @@ public class movement : MonoBehaviour
             PlayerPrefs.SetInt("HighScore", score);
             _nHighScore.GetComponent<TextMeshProUGUI>().text = score.ToString();
         }
+    }
+
+    //FUNGSI MOVEMENT
+      private void bergerak()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        Vector3 gerakan = transform.right * x + transform.forward * z;
+        carkon.Move(gerakan * speed * Time.deltaTime);
+    }
+      private void gravity()
+      {
+        isGround = Physics.CheckSphere(groundcheck.position, ground_Distance, groundMask);
+
+        if(isGround && velocity.y <0){
+            velocity.y = -2f;
+        }
+     
+    }
+     private void jump(){
+        if(Input.GetButtonDown("Jump") && isGround)
+        {
+            velocity.y = Mathf.Sqrt(speed_jump *2f *gravitasi);
+        }
+        velocity.y -= gravitasi * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void lari()
+    {
+        if(Input.GetKey(KeyCode.LeftShift)){
+            speed = speed_lari;
+        }
+        else{
+            speed = 3f;
+        }
+     
     }
 }
---
+++ Assets/Scripts/animasi.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animasi : MonoBehaviour
+{
+    //variable kecepatan
+    private float kecepatan_player;
+    public float nilai_x;
+    public float nilai_z;
+    private bool status_ground;
+
+
+    //referensi
+    private Animator anim;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        nilai_x = player.GetComponent<movement>().x;
+        nilai_z = player.GetComponent<movement>().z;
+        status_ground = player.GetComponent<movement>().isGround;
+        kecepatan_player = player.GetComponent<movement>().speed;
+
+        anim.SetFloat("x", nilai_x);
+        anim.SetFloat("z", nilai_z);
+        anim.SetBool("IsGrounded", status_ground);
+        anim.SetFloat("speed", kecepatan_player);
+
+    }
+}
