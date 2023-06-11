2430a1960f02b7d9a0e872590879f726713410bb
Dicky Darmawan <63191799+dickyd2909@users.noreply.github.com>
2023-06-11 16:23:09 +0700

    menambah script tembakan

--- Assets/Dicky Project/script/ProjectileGuns.cs
+++ Assets/Dicky Project/script/ProjectileGuns.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ProjectileGuns : MonoBehaviour
@@ -14,7 +15,11 @@ public class ProjectileGuns : MonoBehaviour
     bool shooting, readyToShoot, reloading;
 
     public Camera fpsCam;
-    public Transform attacctPoint;
+    public Transform attackPoint;
+
+    public  GameObject muzzleFlash;
+    public TextMeshProGUI ammunitionDisplay;
+
     public bool allowInvoke = true;
 
     private void Awake(){
@@ -24,17 +29,84 @@ public class ProjectileGuns : MonoBehaviour
 
     private void Update(){
         MyInput();
+
+        if(ammunitionDisplay != null){
+            ammunitionDisplay.setText(bulletsLeft / bulletPerTap + " / " + magazineSize / bulletPerTap);
+
+        }
     }
 
     private void MyInput(){
         if(allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
         else shooting = Input.GetKeyDown(KeyCode.Mouse0);
 
+        if(MyInput.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading)Reload();
+
+        if(readyToShoot && shooting && !reloading && bulletsLeft <= 0 ) Reload();
+
         if(readyToShoot && shooting && !reloading && bulletsLeft > 0){
             bulletsShot = 0;
+            Shoot();
         }
     }
 
+    private void Shoot(){
+        readyToShoot = false;
+
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f,0));
+
+        Vector3 targetPoint;
+        if(Pyshics.Raycast(ray, out hit)){
+            targetPoint = hit.point;
+        }else{
+            targetPoint = ray.GetPoint(75);
+        }
+
+        Vector3 diectionWithoutSpread = targetPoint - attackPoint;
+
+        float x = Random.Range(-spread, spread);
+        float y = Random.Range(-spread, spread);
+
+        Vector3 directionWithSpread = diectionWithoutSpread + new Vector3(x,y,0);
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
+
+        currentBullet.transform.forward = directionWithSpread.normalized;
+
+        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+
+        if(muzzleFlash != null){
+           Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity); 
+        }
+
+
+        bulletsLeft--;
+        bulletsShot++;
+        if(allowInvoke){
+            Invoke("ResetShot", timeBeetweenShooting);
+            allowInvoke = false;
+        }
+
+        if(bulletsShot < bulletPerTap && bulletsLeft > 0){
+            Invoke("Shoot", timeBetweenShots);
+        }
+    }
+
+    private voide ResetShoot(){
+        readyToShoot = true;
+        allowInvoke = true;
+    }
+
+    private void Reload(){
+        reloading = true;
+        Invoke("ReloadFinished", reloadTime);
+
+    }
+
+    private void ReloadFinished(){
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
     
 
 }
--- Assets/Dicky Project/script/movement.cs
+++ Assets/Dicky Project/script/movement.cs
@@ -9,17 +9,12 @@ public class movement : MonoBehaviour
     [SerializeField] TextMeshProUGUI _nScore;
     [SerializeField] TextMeshProUGUI _nHighScore;
     public GameObject player;
-    private GameObject menCam;
     private CharacterController carkon;
     private float  highscore,x,y,z;
     private int score;
     // Start is called before the first frame update
     void Start()
     {
-        menCam = Camera.main.gameObject;
-        menCam.transform.parent = player.transform;
-        menCam.transform.localPosition = new Vector3(0.27f, 2f, -2.9f);
-        menCam.transform.localEulerAngles = new Vector3(11, 0, 0);
         carkon = player.GetComponent<CharacterController>();
         score = 0;
         _nScore.GetComponent<TextMeshProUGUI>().text = score.ToString();
@@ -27,18 +22,18 @@ public class movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        bergerak();
-    }
+    // void Update()
+    // {
+    //     bergerak();
+    // }
 
-    private void bergerak()
-    {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
-        Vector3 gerakan = transform.right * x + transform.forward * z;
-        carkon.Move(gerakan * kecepatan * Time.deltaTime);
-    }
+    // private void bergerak()
+    // {
+    //     x = Input.GetAxis("Horizontal");
+    //     z = Input.GetAxis("Vertical");
+    //     Vector3 gerakan = transform.right * x + transform.forward * z;
+    //     carkon.Move(gerakan * kecepatan * Time.deltaTime);
+    // }
 
     private void OnTriggerEnter(Collider other) {
         if(other.tag == "Obstacle")
