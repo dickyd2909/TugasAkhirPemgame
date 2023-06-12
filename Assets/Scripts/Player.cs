09226339c8e54878ed2924b5fa19f1f0d0fa9035
sanydaffahizkil <sanydaffahizkil@gmail.com>
2023-06-12 21:51:43 +0700

    Update MainMenu, Pause

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,35 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    // public static bool loadStatus;
-    // public Player playerInstance;
-    // public GameObject loadingScreens;
-    // public Slider loadingBar;
-    // public TextMeshProUGUI loadingText;
     void Start()
     {
         if(SceneLoader.loadStatus)
         {
             LoadPlayer();
-            
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    
     private void LoadPlayer()
     {
         PlayerData data = SaveSystem.LoadPlayer();
@@ -41,21 +24,6 @@ public class Player : MonoBehaviour
 
         // EnviroSky.instance.SetTime(1, 1, data.time[0], data.time[1], data.time[2]);
         transform.position = position;
-
-
     }
-    // IEnumerator LoadAsync(int sceneIndex)
-    // {
-    //     AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
-
-    //     while(!operation.isDone)
-    //     {
-    //         int progress = (int)Mathf.Clamp01(operation.progress / .9f);
-    //         loadingBar.value = progress;
-    //         loadingText.text = progress  * 100 + "%";
-
-    //         yield return null;
-
-    //     }
-    // }
 }
+
